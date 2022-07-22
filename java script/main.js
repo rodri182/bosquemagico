b893f2bf4390e61681d7ded49812ed4e7843a46d
rodri182 <rodri96mdq@gmail.com>
2022-07-21 22:30:32 -0300

    eventos

--- java script/main.js
+++ java script/main.js
@@ -1,26 +1,4 @@
-console.dir(document);
-console.dir(document.head);
-console.dir(document.body);
 /*
-for (const producto of productos) 
-{
-    let contenedor = document.createElement('div');
-    //Definimos el innerHTML del elemento con una plantilla de texto
-    contenedor.innerHTML = `<h3> ID: ${producto.id}</h3>
-                                <p> Producto: ${producto.nombre}</p>
-                                <b> $ ${producto.precio}</b>`;
-    document.body.appendChild(contenedor);
-}
-const conteiner= document.getElementById("contenedor")
-conteiner.innerHTML="<h3>Hola mundo!</h3><p>Bienvenidos a todos!</p><hr>"
-
-const texto=document.createElement("p")
-texto.innerText="buenassssss hola hola holaaaaaa"
-texto.style="blue bold"
-conteiner.append(texto)
-console.log(conteiner)
-*/
-
 const elementos= ["casa","auto","parque","cochera","mesas"];
 const cosaslist= document.getElementById("cosas-list")
 elementos.forEach((cosas)=>{
@@ -29,3 +7,22 @@ elementos.forEach((cosas)=>{
  list.className = "grey"
  cosaslist.append(list)
 })
+*/
+
+
+function hadleclick ()
+{
+    
+    alert("SU FECHA ESTA SIENDO PROCESADA");
+}
+
+function capturardatos()
+{
+    let nombreadul=document.getElementById("nombreadulto").value;
+    console.log("NOMBRE:    "+ nombreadul + "\n");
+    let fechacumple=document.getElementById("fechareserva").value;
+    console.log("FECHA DEL CUMPLEAÑOS:    "+ fechacumple + "\n" );
+   let nombrecumple=document.getElementById("nombrecumple").value;
+   console.log("CUMPLEAÑERO:        "+nombrecumple )
+
+}
