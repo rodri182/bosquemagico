dfe38856a5a3770539b368016f684623f1b44852
rodri182 <rodri96mdq@gmail.com>
2022-07-28 23:12:42 -0300

    agregando imagenes

--- java script/main.js
+++ java script/main.js
@@ -1,20 +1,12 @@
-/*
-const elementos= ["casa","auto","parque","cochera","mesas"];
-const cosaslist= document.getElementById("cosas-list")
-elementos.forEach((cosas)=>{
- const list=document.createElement("li")
- list.innerHTML = cosas
- list.className = "grey"
- cosaslist.append(list)
-})
-*/
 
-
-function hadleclick ()
-{
-    
-    alert("SU FECHA ESTA SIENDO PROCESADA");
-}
+let nombreadul=localStorage.getItem("Reservado por");
+let fechacumple=localStorage.getItem("Fecha del cumple");
+let nombrecumple=localStorage.getItem("Cumpleaños de");
+let tel=localStorage.getItem("telefono");
+document.getElementById("nombreadulto").value=nombreadul;
+document.getElementById("fechareserva").value=fechacumple;
+document.getElementById("nombrecumple").value=nombrecumple;
+document.getElementById("tel").value=tel;
 
 function capturardatos()
 {
@@ -23,6 +15,12 @@ function capturardatos()
     let fechacumple=document.getElementById("fechareserva").value;
     console.log("FECHA DEL CUMPLEAÑOS:    "+ fechacumple + "\n" );
    let nombrecumple=document.getElementById("nombrecumple").value;
-   console.log("CUMPLEAÑERO:        "+nombrecumple )
-
+   console.log("CUMPLEAÑERO:        "+nombrecumple );
+    let tel=document.getElementById("tel").value;
+   localStorage.setItem("Reservado por",nombreadul);
+   localStorage.setItem("Fecha del cumple",fechacumple);
+   localStorage.setItem("Cumpleaños de",nombrecumple);
+   localStorage.setItem("telefono", tel);
+   alert("SU FECHA ESTA SIENDO PROCESADA");
 }
+
