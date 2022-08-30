bb96e5d6fbf26a00d0b0d8385931a5ba753d416d
rodri182 <rodri96mdq@gmail.com>
2022-08-29 22:34:10 -0300

    proyecto final

--- java script/imagenes.js
+++ java script/imagenes.js
@@ -1,8 +1,25 @@
 
+(async function() {
+    const response = await fetch('./json/galeria.json')
+    const data = await response.json()
+
+    for(let cont = 0; cont < data.length; cont++) {
+      
+        const gallery = document.querySelector('.contenedor-galeria')
+        const newImg = document.createElement('img')
+
+        gallery.appendChild(newImg)
+
+        newImg.setAttribute("class", "img-galeria")
+        newImg.setAttribute("src", data[cont].src)
+        newImg.setAttribute("alt", data[cont].alt)
+        console.log("chau")
+    }
+   
 const imagenes= document.querySelectorAll(".img-galeria")
 const imagenesLigth=document.querySelector(".agregar-img")
 const contenedorLigth=document.querySelector(".imagen-ligth")
-
+console.log("hola");
 imagenes.forEach(imagen=>{
     imagen.addEventListener("click",()=>{
         aparecerImg(imagen.getAttribute("src"))
@@ -18,8 +35,10 @@ contenedorLigth.addEventListener("click", (flecha)=>{
     }
 })
 const aparecerImg=(imagen)=>{
-
     imagenesLigth.src=imagen;
     contenedorLigth.classList.toggle("show")
     imagenesLigth.classList.toggle("showImage")
 }
+
+})()
+
--- java script/main.js
+++ java script/main.js
@@ -1,38 +1,63 @@
 
-let nombreadul=localStorage.getItem("Reservado por");
-let fechacumple=localStorage.getItem("Fecha del cumple");
-let nombrecumple=localStorage.getItem("Cumpleaños de");
-let tel=localStorage.getItem("telefono");
-document.getElementById("nombreadulto").value=nombreadul;
-document.getElementById("fechareserva").value=fechacumple;
-document.getElementById("nombrecumple").value=nombrecumple;
-document.getElementById("tel").value=tel;
+document.getElementById("nombreAdulto").value = 'nombre';
+document.getElementById("fechaReserva").value = 'fechaCumple';
+document.getElementById("nombreCumple").value = 'cumpleañero';
+document.getElementById("tel").value = 'tel';
 
-function capturardatos()
-{
-    let nombreadul=document.getElementById("nombreadulto").value;
-    console.log("NOMBRE:    "+ nombreadul + "\n");
-    let fechacumple=document.getElementById("fechareserva").value;
-    console.log("FECHA DEL CUMPLEAÑOS:    "+ fechacumple + "\n" );
-   let nombrecumple=document.getElementById("nombrecumple").value;
-   console.log("CUMPLEAÑERO:        "+nombrecumple );
-    let tel=document.getElementById("tel").value;
-   localStorage.setItem("Reservado por",nombreadul);
-   localStorage.setItem("Fecha del cumple",fechacumple);
-   localStorage.setItem("Cumpleaños de",nombrecumple);
-   localStorage.setItem("telefono", tel);
-   swal.fire({
-    title: "Listo!",
-    width:600,
-    text:"Su fecha se esta procesando",
-    imageUrl:"./imagenes/globos.png",
-    imageWidth: 250,
-    ImageHeigth: 80,
-    imageAlt: "img-catel",
-    color:"#27e83e",
-    background:"#f7f6f6",
-    confirmButtonText:"aceptar",
-   });
+
+function capturardatos() {
+    const fechaReserva = document.getElementById("fechaReserva").value;
+    const nombre = document.getElementById("nombreAdulto").value;
+    const fecha = document.getElementById("fechaReserva").value;
+    const cumpleanero = document.getElementById("nombreCumple").value;
+    const telefono = document.getElementById("tel").value;
+
+    const objetocumple = {
+        nombre,
+        cumpleanero,
+        telefono,
+        fecha,
+        fechaReserva,
+    };
+    
+
+    const todosMisCumpleaniosString = localStorage.getItem("cumple") || '[]';
+    const todosMisCumpleanios = JSON.parse(todosMisCumpleaniosString);
+   
+    const existeLaFecha = todosMisCumpleanios.findIndex(cum => cum.fechaReserva == fechaReserva)
+    
+    if(existeLaFecha >= 0) {
+        swal.fire({
+            title: "La fecha ya existe!",
+            width:600,
+            text:"Elija otra fecha",
+            imageUrl:"./imagenes/globos.png",
+            imageWidth: 250,
+            ImageHeigth: 80,
+            imageAlt: "img-catel",
+            color:"red",
+            background:"white",
+            confirmButtonText:"aceptar",
+        });
+        return
+    }
+
+    todosMisCumpleanios.push(objetocumple);
+    const cumpleanito = JSON.stringify(todosMisCumpleanios);
+    localStorage.setItem("cumple", cumpleanito);
+
+    swal.fire({
+        title: "Listo!",
+        width:600,
+        text:"Su fecha se esta procesando",
+        imageUrl:"./imagenes/globos.png",
+        imageWidth: 250,
+        ImageHeigth: 80,
+        imageAlt: "img-catel",
+        color:"#27e83e",
+        background:"#f7f6f6",
+        confirmButtonText:"aceptar",
+    });
 }
 
 
